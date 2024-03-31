9d3b6e17d518eea602c3722544cd087feadea54c
Francois Mizrahi <francois.mizrahi@gmail.com>
2024-03-31 17:51:41 +0200

    first main page

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 import streamlit as st
+from transformers import pipeline
+
+st.set_page_config(layout="wide")
+
+leftcol, maincol, rightcol = st.columns([1, 4, 1])
+
+with maincol:
+    st.title("Porfolio")
+
+    classifier = pipeline("sentiment-analysis")
+    st.write(classifier("I've been waiting for a HuggingFace course my whole life."))
