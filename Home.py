5170c50d939aec560a2e75b3c701aae8c3cc3e61
Francois Mizrahi <francois.mizrahi@gmail.com>
2024-05-26 21:52:45 +0200

    Change projects files names

--- Home.py
+++ Home.py
@@ -12,11 +12,13 @@ leftcol, maincol, rightcol = st.columns([1, 10, 1])
 
 # with rightcol:
     # st.image('images/portfolio_small.png')
+st.sidebar.markdown("Use the links below to navigate:")
 
 with maincol:
     multi = """
     <div style="text-align: left;">
     <h1>Hi, I'm Francois Mizrahi</h1>
+    <p>Hey there, I'm Francois Mizrahi! I'm passionate about data and technology. My expertise covers a wide range of topics, from Data Analytics and Data Engineering to Data Science, Operation Research, Product Management and more</p>
     </div>
     """
     st.markdown(multi, unsafe_allow_html=True)
